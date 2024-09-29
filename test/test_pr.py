eea0d51b76c6fd6b733e2eb0f8ef5905102006ea
ulrik-s <ulrik.sjolin@sensative.com>
2024-09-29 20:10:40 +0200

    Add tests

---
+++ test/test_pr.py
@@ -0,0 +1,8 @@
+from click.testing import CliRunner
+from fp_py import cli
+
+def test_pr():
+  runner = CliRunner()
+  result = runner.invoke(cli, ['pr'])
+  assert result.exit_code == 0
+  assert result.output == 'Hello World\n'
